edeeed301f1b1ba8a20d9f8b2006fa7df7e874f5
hpmtissera <hpmtissera@gmail.com>
2016-11-20 19:45:24 +0900

    Fixed #TRIP-1 : Configured Spring MVC with Gradle and Spring Boot Fixed #TRIP-2 : Configured Spring web app with Thymeleaf with Spring Boot

---
+++ src/main/java/com/govoyage/tripplanner/GreetingController.java
@@ -0,0 +1,20 @@
+package com.govoyage.tripplanner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by prasad on 2016/11/20.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
