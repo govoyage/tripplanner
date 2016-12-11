9e902e709ac245a0d12c834b99633f596ec71b7c
hpmtissera <hpmtissera@gmail.com>
2016-12-11 18:00:46 +0900

    Configure embeded h2 database with Spring Data #TRIP-3 fixed, #TRIP-4 fixed

--- src/main/java/com/govoyage/tripplanner/GreetingController.java
+++ src/main/java/com/govoyage/tripplanner/GreetingController.java
@@ -1,5 +1,8 @@
 package com.govoyage.tripplanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +14,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    private static final Logger log = LoggerFactory.getLogger(TripPlanner.class);
+
+    @Autowired
+    CustomerRepository repository;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
+        String msg = "Nothing persisted";
+        if (!"World".equals(name)) {
+            msg = persistCustomer(name);
+        }
+        model.addAttribute("msg", msg);
         return "greeting";
+    }
+
+    public String persistCustomer(String name) {
+        StringBuilder sb = new StringBuilder();
+        repository.save(new Customer(name, name));
+        log.info("Saved successfully");
+        Customer customer = repository.findByLastName(name).get(0);
+        String msg = "Loaded customer : " + customer.toString();
+        log.info(msg);
+        sb.append(msg);
+        return sb.toString();
     }
 
 }
